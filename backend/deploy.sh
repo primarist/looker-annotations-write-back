f5439835b7f5de7fe581686eee804c0da035a724
Tristan Foureur <tristan.foureur@gmail.com>
2023-12-06 16:11:22 +0100

    deploy file

--- backend/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-rsync -avz . esya@10.0.0.10:/home/esya/looker-hackathon --exclude node_modules --exclude .git
-ssh esya@10.0.0.10 "cd /home/esya/looker-hackathon && npm install && npm run build && /home/esya/.npm-global/bin/pm2 restart all"
